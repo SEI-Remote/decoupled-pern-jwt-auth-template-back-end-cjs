74e381eb440508216fc290c6ab26278376101aaf
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2023-02-12 18:19:16 -0600

    fix user deletion bug

--- models/profile.js
+++ models/profile.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Profile.belongsTo(models.User, { as: "user", foreignKey: 'userId' })
+      Profile.belongsTo(models.User, { as: 'user', foreignKey: 'userId' })
     }
   }
   
